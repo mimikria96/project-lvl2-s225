ec1c0c8c65fd6f290d8e3ee731d18c803d8ed3a9
mimikria96 <chemiakina7493@mail.ru>
2018-03-14 22:54:42 +0300

    add moduls and simplify parser

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {programCli} from '..';
+import programCli from '../parser';
 
 
 programCli();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import program from 'commander';
-import * as _ from 'lodash';
+import  _ from 'lodash';
 import parseFile from './parser';
 
-export const genDiff = (fileBefore, fileAfter) => {
+const genDiff = (fileBefore, fileAfter) => {
   const objBefore = parseFile(fileBefore);
   const objAfter = parseFile(fileAfter);
   const keys = _.union(Object.keys(objBefore), Object.keys(objAfter));
@@ -19,14 +18,6 @@ export const genDiff = (fileBefore, fileAfter) => {
     return [...acc,` + ${key}: ${objAfter[key]}\n - ${key}: ${objBefore[key]}`];
   }, []).join('\n');
   return result;
-}
-
-export const programCli = () => {
-  return program
-    .version('0.1.0')
-    .description(`Compares two configuration files and shows a difference.`)
-    .arguments('<firstConfig> <secondConfig>')
-    .option('-f, --format [type]', 'Output format')
-    .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)))
-    .parse(process.argv);
 };
+
+export default genDiff;
