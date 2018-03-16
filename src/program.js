f118abde49daeb891d3ccaee00500f326e37e6d6
mimikria96 <chemiakina7493@mail.ru>
2018-03-16 16:36:43 +0300

    change action

--- src/program.js
+++ src/program.js
@@ -5,9 +5,9 @@ const programCli = () => {
   return program
     .version('0.1.0')
     .description(`Compares two configuration files and shows a difference.`)
-    .option('-f, --format [type]', 'Output format')
     .arguments('[format] <firstConfig> <secondConfig>')
-    .action((format, firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig, format)))
+    .option('-f, --format [type]', 'Output format')
+    .action((type, firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig, type)))
     .parse(process.argv);
 };
 export default programCli;
